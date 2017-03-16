e8f9f7ac465288b2944118fb715ad7755f988fce
Jorge Santiago Alvarez Cuadros <sanjorgek@ciencias.unam.mx>
2017-03-16 17:01:56 -0600

    initialized

--- routesPermission/index.js
+++ routesPermission/index.js
@@ -3,10 +3,22 @@
  */
 
 module.exports = function (sails) {
+  function permissionRoutes (route, cb) {
+    for (var index in sails.permissions.routesRE) {
+      if (sails.permissions.routesRE.hasOwnProperty(index)) {
+        var routeRE = sails.permissions.routesRE[index];
+        var routeMatcher = new RegExp(routeRE.replace(/:[^\s/]+/g, '([\\w-]+)'));
+        var match = route.match(routeMatcher);
+        if(match!=null) return cb(null, sails.permissions.restrictions[routeRE]);
+      }
+    }
+    return cb(sails.errorhandler.create("unauthorized", "Route not found in permissions"));
+  };
+  
   return {
     
     defaults: {
-      routesPermission : {}
+      routesPermissions : {}
     },
 
     //configure: function(){},
@@ -19,7 +31,12 @@ module.exports = function (sails) {
     // Run when sails loads-- be sure and call `next()`.
     initialize: function (next) {
       sails.after(["hook:orm:loaded", "hook:errorhandler:loaded"], function() {
-        return next();
+        sails.permissions = {
+          routesRE : Object.keys(sails.config.routesPermissions),
+          restrictions : sails.config.routesPermissions,
+          permissionRoutes : permissionRoutes
+        };
+        next();
       });
     }
 
